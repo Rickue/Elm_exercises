480a9561ff04d6b577fed390a258b47787890486
Richy <richyk99@web.de>
2026-06-03 16:47:06 +0200

    Initial Elm assignment commit

---
+++ Elm_exercises/exercise_7.5/src/Main.elm
@@ -0,0 +1,371 @@
+module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
+
+import Browser
+import Color
+import Csv
+import Csv.Decode
+import Date exposing (Date)
+import Dict exposing (Dict)
+import Html exposing (Html, div, pre, text)
+import Http
+import RecursivePattern exposing (Level(..), PixelPositon(..), RecordedData(..), augementLevel, createPixelMap, startPosition)
+import RecursivePattern.Helper exposing (drawTuplePosition)
+import Scale
+import Scale.Color
+import Statistics
+import String
+import TypedSvg
+import TypedSvg.Attributes
+import TypedSvg.Core
+import TypedSvg.Types
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+dataUrl : String
+dataUrl =
+    "../data_csv/DJ.csv"
+
+
+type alias Series =
+    List ( String, Maybe Float )
+
+
+type alias CurrentRecordedData msg =
+    RecordedData ( String, Maybe Float ) (List (TypedSvg.Core.Attribute msg))
+
+
+type Model
+    = Failure
+    | Loading
+    | Success
+        { raw : Series
+        , expanded : Series
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = dataUrl
+        , expect = Http.expectString GotText
+        }
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg _ =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    let
+                        rawSeries =
+                            csvString_to_data fullText
+
+                        expandedSeries =
+                            expandSeries rawSeries
+                    in
+                    ( Success
+                        { raw = rawSeries
+                        , expanded = expandedSeries
+                        }
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "I was unable to load DJ.csv."
+
+        Loading ->
+            text "Loading..."
+
+        Success series ->
+            let
+                level =
+                    [ Level 5 1
+                    , Level 1 12
+                    , Level 4 1
+                    , Level 2 3
+                    , Level 3 3
+                    , Level 1 1
+                    ]
+
+                ( w, h ) =
+                    ( 500, 500 )
+
+                currentValues : List Float
+                currentValues =
+                    series.expanded
+                        |> List.filterMap Tuple.second
+
+                normalize =
+                    currentValues
+                        |> Statistics.extent
+                        |> Maybe.withDefault ( 0, 1 )
+                        |> Scale.linear ( 0, 1 )
+
+                ( minValue, maxValue ) =
+                    currentValues
+                        |> Statistics.extent
+                        |> Maybe.withDefault ( 0, 1 )
+
+                pixelList : List PixelPositon
+                pixelList =
+                    createPixelMap startPosition (augementLevel level)
+
+                ourData : List (CurrentRecordedData Msg)
+                ourData =
+                    List.map2 (\a b -> RecordedData a b []) pixelList series.expanded
+
+                drawPosition : CurrentRecordedData Msg -> CurrentRecordedData Msg
+                drawPosition (RecordedData pixelPosition value _) =
+                    RecordedData
+                        pixelPosition
+                        value
+                        (drawTuplePosition ( w, h ) level pixelPosition)
+
+                createStyle : ( String, Maybe Float ) -> List (TypedSvg.Core.Attribute Msg)
+                createStyle ( dateString, value ) =
+                    let
+                        colorValue =
+                            case value of
+                                Just v ->
+                                    Scale.Color.redYellowGreenInterpolator (1 - Scale.convert normalize v)
+
+                                Nothing ->
+                                    Color.lightGray
+                    in
+                    [ TypedSvg.Attributes.title
+                        (if String.isEmpty dateString then
+                            "Missing date"
+
+                         else
+                            dateString
+                        )
+                    , TypedSvg.Attributes.fill (TypedSvg.Types.Paint colorValue)
+                    , TypedSvg.Attributes.stroke (TypedSvg.Types.Paint (Color.rgb255 244 244 244))
+                    , TypedSvg.Attributes.strokeWidth (TypedSvg.Types.px 0.35)
+                    , TypedSvg.Attributes.shapeRendering TypedSvg.Types.RenderCrispEdges
+                    ]
+
+                drawStyle : CurrentRecordedData Msg -> CurrentRecordedData Msg
+                drawStyle (RecordedData pixelPosition value attributeList) =
+                    RecordedData
+                        pixelPosition
+                        value
+                        (List.append attributeList (createStyle value))
+
+                draw : CurrentRecordedData Msg -> TypedSvg.Core.Svg Msg
+                draw (RecordedData _ _ attributeList) =
+                    TypedSvg.rect
+                        attributeList
+                        []
+
+                entryToLine : CurrentRecordedData msg -> String
+                entryToLine (RecordedData pixelPos ( dateString, maybeValue ) _) =
+                    let
+                        posText =
+                            case pixelPos of
+                                PixelPositon x y ->
+                                    "position " ++ String.fromInt x ++ ", " ++ String.fromInt y
+
+                        valueText =
+                            case maybeValue of
+                                Just v ->
+                                    String.fromFloat v
+
+                                Nothing ->
+                                    "Nothing"
+                    in
+                    dateString ++ ", " ++ valueText ++ " to " ++ posText
+            in
+            div []
+                [ TypedSvg.svg
+                    [ TypedSvg.Attributes.viewBox 0 0 500 590
+                    , TypedSvg.Attributes.width (TypedSvg.Types.px 800)
+                    , TypedSvg.Attributes.height (TypedSvg.Types.px 944)
+                    , TypedSvg.Attributes.preserveAspectRatio
+                        (TypedSvg.Types.Align TypedSvg.Types.ScaleMin TypedSvg.Types.ScaleMin)
+                        TypedSvg.Types.Meet
+                    ]
+                    [ TypedSvg.text_
+                        [ TypedSvg.Attributes.x (TypedSvg.Types.px 8)
+                        , TypedSvg.Attributes.y (TypedSvg.Types.px 18)
+                        , TypedSvg.Attributes.fontSize (TypedSvg.Types.px 16)
+                        , TypedSvg.Attributes.fontWeight TypedSvg.Types.FontWeightBold
+                        , TypedSvg.Attributes.fill (TypedSvg.Types.Paint Color.black)
+                        ]
+                        [ TypedSvg.Core.text
+                            ("Loaded: DJ(" ++ String.fromInt (List.length series.raw) ++ ")")
+                        ]
+                    , TypedSvg.g
+                        [ TypedSvg.Attributes.transform [ TypedSvg.Types.Translate 0 76 ] ]
+                        (List.map (drawPosition >> drawStyle >> draw) ourData)
+                    , TypedSvg.g
+                        [ TypedSvg.Attributes.transform [ TypedSvg.Types.Translate 0 50 ] ]
+                        [ TypedSvg.rect
+                            [ TypedSvg.Attributes.x (TypedSvg.Types.px 8)
+                            , TypedSvg.Attributes.y (TypedSvg.Types.px 0)
+                            , TypedSvg.Attributes.width (TypedSvg.Types.px 18)
+                            , TypedSvg.Attributes.height (TypedSvg.Types.px 6)
+                            , TypedSvg.Attributes.fill
+                                (TypedSvg.Types.Paint (Scale.Color.redYellowGreenInterpolator 1))
+                            ]
+                            []
+                        , TypedSvg.rect
+                            [ TypedSvg.Attributes.x (TypedSvg.Types.px 28)
+                            , TypedSvg.Attributes.y (TypedSvg.Types.px 0)
+                            , TypedSvg.Attributes.width (TypedSvg.Types.px 18)
+                            , TypedSvg.Attributes.height (TypedSvg.Types.px 6)
+                            , TypedSvg.Attributes.fill
+                                (TypedSvg.Types.Paint (Scale.Color.redYellowGreenInterpolator 0.5))
+                            ]
+                            []
+                        , TypedSvg.rect
+                            [ TypedSvg.Attributes.x (TypedSvg.Types.px 48)
+                            , TypedSvg.Attributes.y (TypedSvg.Types.px 0)
+                            , TypedSvg.Attributes.width (TypedSvg.Types.px 18)
+                            , TypedSvg.Attributes.height (TypedSvg.Types.px 6)
+                            , TypedSvg.Attributes.fill
+                                (TypedSvg.Types.Paint (Scale.Color.redYellowGreenInterpolator 0))
+                            ]
+                            []
+                        , TypedSvg.text_
+                            [ TypedSvg.Attributes.x (TypedSvg.Types.px 72)
+                            , TypedSvg.Attributes.y (TypedSvg.Types.px 6)
+                            , TypedSvg.Attributes.fontSize (TypedSvg.Types.px 10)
+                            , TypedSvg.Attributes.fill (TypedSvg.Types.Paint Color.black)
+                            ]
+                            [ TypedSvg.Core.text
+                                ("Open (index value): min "
+                                    ++ String.fromFloat minValue
+                                    ++ "  max "
+                                    ++ String.fromFloat maxValue
+                                )
+                            ]
+                        , TypedSvg.text_
+                            [ TypedSvg.Attributes.x (TypedSvg.Types.px 72)
+                            , TypedSvg.Attributes.y (TypedSvg.Types.px 18)
+                            , TypedSvg.Attributes.fontSize (TypedSvg.Types.px 10)
+                            , TypedSvg.Attributes.fill (TypedSvg.Types.Paint Color.black)
+                            ]
+                            [ TypedSvg.Core.text "Grün = low, Rot = high, Grau = closed" ]
+                        ]
+                    ]
+                , pre []
+                    [ text
+                        (ourData
+                            |> List.map entryToLine
+                            |> String.join "\n"
+                        )
+                    ]
+                ]
+
+
+csvString_to_data : String -> List ( String, Maybe Float )
+csvString_to_data csvRaw =
+    Csv.parse csvRaw
+        |> Csv.Decode.decodeCsv decodeStockDay
+        |> Result.toMaybe
+        |> Maybe.withDefault []
+
+
+expandSeries : Series -> Series
+expandSeries series =
+    Dict.union (Dict.fromList series) holidayDict
+        |> Dict.toList
+
+
+holidayDict : Dict String (Maybe Float)
+holidayDict =
+    Date.range Date.Day 1 startDate endDate
+        |> List.map (\dateValue -> ( Date.toIsoString dateValue, Nothing ))
+        |> Dict.fromList
+
+
+startDate : Date
+startDate =
+    Date.fromIsoString "1980-12-23"
+        |> Result.withDefault (Date.fromRataDie 0)
+
+
+endDate : Date
+endDate =
+    Date.fromIsoString "2011-06-09"
+        |> Result.withDefault (Date.fromRataDie 0)
+
+
+decodeStockDay : Csv.Decode.Decoder (( String, Maybe Float ) -> a) a
+decodeStockDay =
+    Csv.Decode.map (\date open -> ( date, Just open ))
+        (Csv.Decode.field "Date" Ok
+            |> Csv.Decode.andMap
+                (Csv.Decode.field "Open"
+                    (String.toFloat >> Result.fromMaybe "error parsing string")
+                )
+        )
+
+
+formatEntry : ( String, Maybe Float ) -> String
+formatEntry ( date, openValue ) =
+    let
+        dateText =
+            if String.isEmpty date then
+                "Missing date"
+
+            else
+                date
+
+        openText =
+            case openValue of
+                Just value ->
+                    String.fromFloat value
+
+                Nothing ->
+                    "Nothing"
+    in
+    dateText ++ ": " ++ openText
---
+++ Elm_exercises/lib/recursive-pattern/src/RecursivePattern.elm
@@ -0,0 +1,227 @@
+module RecursivePattern exposing (Level(..), PixelPositon(..), RecordedData(..), augementLevel, createPixelMap, createPixelMapTopDown, createPixelMapZ, startPosition)
+
+{-| This library provides the option of creating a list of pixel positions based on a specific order
+
+
+# Definition
+
+@docs RecursivePattern
+
+-}
+
+
+{-| type definition for the data and results used
+get data and result type to create useable type
+useable for chaning optional position functions
+-}
+type RecordedData value result
+    = RecordedData PixelPositon value result
+
+
+{-| helper type for the recursive loop call
+xOuterLoop -> x value of the outer loop
+yOuterLoop -> y value of the outer loop
+outerResult -> intermediate results
+-}
+type alias LoopRec =
+    { xOuterLoop : Int, yOuterLoop : Int, outerResult : List PixelPositon }
+
+
+{-| type definition for a pixel position
+first parameter -> defines the horizontal position of the pixel
+second parameter -> defines the vertical position of the pixel
+-}
+type PixelPositon
+    = PixelPositon Int Int
+
+
+{-| type definition for a recursive pattern level
+first parameter -> defines the number of pixels horizontally
+second parameter -> defines the number of pixels vertically
+-}
+type Level
+    = Level Int Int
+
+
+{-| constructor for the start value of the pixel positions
+affects the position within the representation
+
+    startPosition == PixelPositon 0 0
+
+-}
+startPosition : PixelPositon
+startPosition =
+    PixelPositon 0 0
+
+
+{-| creates the augment level -> takes the last level and multiply them on the current level
+
+    augmentLevel [ Level 2 2 ] == [ ( Level 2 2, Level 1 1 ), ( Level 1 1, Level 0 0 ) ]
+
+-}
+augementLevel : List Level -> List ( Level, Level )
+augementLevel level =
+    let
+        usableLevel =
+            level ++ [ Level 1 1 ]
+
+        next : List Level
+        next =
+            List.foldr
+                -- change to foldl
+                (\(Level currentLevelW currentLevelH) nextLevel ->
+                    let
+                        (Level one two) =
+                            case nextLevel of
+                                [] ->
+                                    Level 1 1
+
+                                (Level x y) :: _ ->
+                                    Level x y
+                    in
+                    Level (currentLevelW * one) (currentLevelH * two) :: nextLevel
+                )
+                []
+                usableLevel
+    in
+    List.map2 Tuple.pair usableLevel (List.append (List.drop 1 next) [ Level 0 0 ])
+
+
+{-| creates a list of pixel positions that correspond to the left-right arrangement
+
+    createPixelMap (PixelPosition 0 0) [ ( Level 2 2, Level 1 1 ), ( Level 1 1, Level 0 0 ) ] == [ PixelPositon 0 0, PixelPositon 1 0, PixelPositon 0 1, PixelPositon 1 1 ]
+
+-}
+createPixelMap : PixelPositon -> List ( Level, Level ) -> List PixelPositon
+createPixelMap (PixelPositon x y) level =
+    case level of
+        [] ->
+            [ PixelPositon x y ]
+
+        ( Level w h, Level nextW nextH ) :: reducedLevel ->
+            let
+                temp =
+                    List.range 1 h
+                        |> List.foldl
+                            (\_ { xOuterLoop, yOuterLoop, outerResult } ->
+                                let
+                                    innerResult =
+                                        List.range 1 w
+                                            |> List.foldl
+                                                (\_ ( xLoop, result ) ->
+                                                    ( xLoop + nextW
+                                                    , List.append
+                                                        result
+                                                        (createPixelMap (PixelPositon xLoop yOuterLoop) reducedLevel)
+                                                    )
+                                                )
+                                                ( xOuterLoop, [] )
+                                in
+                                LoopRec
+                                    xOuterLoop
+                                    (yOuterLoop + nextH)
+                                    (List.append outerResult (Tuple.second innerResult))
+                            )
+                            (LoopRec x y [])
+            in
+            temp.outerResult
+
+
+
+{-| creates a list of pixel positions that correspond to the top-down arrangement
+
+    createPixelMapTopDown (PixelPosition 0 0) [ ( Level 2 2, Level 1 1 ), ( Level 1 1, Level 0 0 ) ] == [ PixelPositon 0 0, PixelPositon 0 1, PixelPositon 1 0, PixelPositon 1 1 ]
+
+-}
+createPixelMapTopDown : PixelPositon -> List ( Level, Level ) -> List PixelPositon
+createPixelMapTopDown (PixelPositon x y) level =
+    case level of
+        [] ->
+            [ PixelPositon x y ]
+
+        ( Level w h, Level nextW nextH ) :: reducedLevel ->
+            let
+                temp =
+                    List.range 1 w
+                        |> List.foldl
+                            (\_ { xOuterLoop, yOuterLoop, outerResult } ->
+                                let
+                                    innerResult =
+                                        List.range 1 h
+                                            |> List.foldl
+                                                (\_ ( yLoop, result ) ->
+                                                    ( yLoop + nextH
+                                                    , List.append
+                                                        result
+                                                        (createPixelMapTopDown (PixelPositon xOuterLoop yLoop) reducedLevel)
+                                                    )
+                                                )
+                                                ( yOuterLoop, [] )
+                                in
+                                LoopRec
+                                    (xOuterLoop + nextW)
+                                    yOuterLoop
+                                    (List.append outerResult (Tuple.second innerResult))
+                            )
+                            (LoopRec x y [])
+            in
+            temp.outerResult
+
+
+
+{-| creates a list of pixel positions that correspond to the back-and-forth arrangement
+
+    createPixelMapZ (PixelPosition 0 0) [ ( Level 2 2, Level 1 1 ), ( Level 1 1, Level 0 0 ) ] == [ PixelPositon 0 0, PixelPositon 1 0, PixelPositon 1 1, PixelPositon 0 1 ]
+
+-}
+createPixelMapZ : PixelPositon -> List ( Level, Level ) -> List PixelPositon
+createPixelMapZ (PixelPositon x y) level =
+    case level of
+        [] ->
+            [ PixelPositon x y ]
+
+        ( Level w h, Level nextW nextH ) :: reducedLevel ->
+            let
+                temp =
+                    List.range 1 h
+                        |> List.foldl
+                            (\height { xOuterLoop, yOuterLoop, outerResult } ->
+                                let
+                                    isOdd =
+                                        if h == 1 && nextH == 1 then
+                                            modBy 2 yOuterLoop == 1
+
+                                        else
+                                            modBy 2 height == 0
+
+                                    innerResult =
+                                        List.range 1 w
+                                            |> List.foldl
+                                                (\_ ( xLoop, result ) ->
+                                                    let
+                                                        xWert =
+                                                            if isOdd then
+                                                                xLoop - nextW
+
+                                                            else
+                                                                xLoop + nextW
+
+                                                        nextX =
+                                                            if isOdd then
+                                                                xLoop + ((w - 1) * nextW)
+
+                                                            else
+                                                                xLoop
+                                                    in
+                                                    ( xWert, List.append result (createPixelMapZ (PixelPositon nextX yOuterLoop) reducedLevel) )
+                                                )
+                                                ( xOuterLoop, [] )
+                                in
+                                LoopRec
+                                    xOuterLoop
+                                    (yOuterLoop + nextH)
+                                    (List.append outerResult (Tuple.second innerResult))
+                            )
+                            (LoopRec x y [])
+            in
+            temp.outerResult
